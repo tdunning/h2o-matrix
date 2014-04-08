e52fde6d5df57b04a56fe326f91e534c51a2d818
Anand Avati <avati@redhat.com>
2014-04-08 12:07:45 -0700

    algo: boilerplate for KMeans
    Signed-off-by: Anand Avati <avati@redhat.com>

---
+++ src/main/java/com/oxdata/algo/KMeans.java
@@ -0,0 +1,18 @@
+package com.oxdata.algo;
+
+import com.oxdata.math.H2OMatrix;
+import java.io.File;
+
+public class KMeans {
+    public static void main(String args[]) {
+	H2OMatrix matrix;
+
+	if (args.length != 2) {
+	    System.out.println ("Usage: Kmeans <csv> <nCentroid>");
+	    System.exit(0);
+	}
+
+	System.out.println("File: " + args[0]);
+	matrix = new H2OMatrix(new File(args[0]));
+    }
+}
